7d1fa4e4cf66b79eb603fb1afc07298e5097d932
tommy <tommyrheo@gmail.com>
2022-09-03 19:52:15 +0900

    サーバー

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,99 @@
+use rand::rngs::ThreadRng;
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+
 fn main() {
-    let h = 10;
-    let w = 10;
-    let me = (4, 4);
-    let opp = (6, 6);
+    let mut v = vec![-1; 100];
+    v[4 * 10 + 4] = 0;
+    v[6 * 10 + 6] = 1;
+    let req = Request {
+        size: Point { x: 10, y: 10 },
+        player_pos: Point { x: 4, y: 4 },
+        ai_pos: Point { x: 6, y: 6 },
+        board: v,
+    };
+    let s = serde_json::to_string(&req).unwrap();
+    println!("{}", s);
+    println!("{:?}", s.as_bytes());
+
+    let listener = TcpListener::bind("localhost:6583").unwrap();
+    println!("Listening on localhost:6583");
+
+    for stream in listener.incoming() {
+        match stream {
+            Err(e) => eprintln!("{:?}", e),
+            Ok(stream) => {
+                thread::spawn(move || handler(stream).unwrap_or_else(|e| eprintln!("{:?}", e)));
+                stderr().flush().unwrap();
+            }
+        }
+    }
+}
+
+fn handler(mut stream: TcpStream) -> Result<(), String> {
+    println!("Accessed!");
+    stream
+        .set_read_timeout(Some(Duration::from_millis(500)))
+        .map_err(|e| e.to_string())?;
+    let mut buf = [0u8; 4096];
+    stream.read(&mut buf).map_err(|e| e.to_string())?;
+    let s = String::from_utf8(buf.to_vec().into_iter().take_while(|k| *k != 0).collect())
+        .map_err(|e| e.to_string())?;
+    let s = s.trim().lines().nth(5).unwrap().trim();
+    let req: Request = serde_json::from_str(&s).unwrap();
+
+    let state = Board::from_request(req);
+    let op = state.min_max(10);
+    let res = serde_json::to_string(&op)
+        .map_err(|e| e.to_string())
+        .unwrap();
+    stream.write(res.as_bytes()).map_err(|e| e.to_string())?;
+    stream.flush().map_err(|e| e.to_string())
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+struct Request {
+    size: Point,
+    player_pos: Point,
+    ai_pos: Point,
+    board: Vec<isize>,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+fn cui() {
+    let mut rng = ThreadRng::default();
+    let h = rng.gen_range(7..=15);
+    let w = rng.gen_range(7..=15);
+    let me = (rng.gen_range(1..h - 1), rng.gen_range(1..w - 1));
+    let opp = (rng.gen_range(1..h - 1), rng.gen_range(1..w - 1));
+
+    if me == opp {
+        println!("やばい！");
+        return;
+    }
 
     let mut board = Board::new(h, w, me, opp, 0);
+
     println!("{}", board);
 
     while !board.enumerate().is_empty() {
-        let op = if board.n == 0 {
+        let player_turn = board.n == 0;
+        let op = if player_turn {
             decide_by_input()
         } else {
-            board.min_max(12)
+            board.min_max(15)
         };
 
-        board.operate(op).unwrap();
+        let res = board.operate(op);
+        if res.is_err() && player_turn {
+            println!("Invalid Input!!!");
+            continue;
+        }
 
         println!("{}", board);
     }
@@ -34,21 +113,26 @@ fn decide_by_input() -> Operation {
         's' => Down,
         'd' => Right,
         'a' => Left,
-        _ => unreachable!(),
+        _ => {
+            println!("Invalid Input!");
+            decide_by_input()
+        }
     }
 }
 
+use std::io::{stderr, BufRead, BufReader, Read};
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
 use std::{
     collections::VecDeque,
     fmt::Display,
     io::{stdin, stdout, Write},
     thread,
-    time::Duration,
 };
 
+use rayon::prelude::*;
 use Operation::*;
 use Piece::*;
-use rayon::prelude::*;
 
 const INF: usize = 1001001001001001001;
 
@@ -80,13 +164,23 @@ fn next_op(pos: Position, op: Operation, h: usize, w: usize) -> Option<Position>
     }
 }
 
+fn prev_op(pos: Position, op: Operation, h: usize, w: usize) -> Option<Position> {
+    let op = match op {
+        Up => Down,
+        Down => Up,
+        Right => Left,
+        Left => Right,
+    };
+    next_op(pos, op, h, w)
+}
+
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum Piece {
     Empty,
     Player(usize),
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 enum Operation {
     Up,
     Down,
@@ -99,6 +193,7 @@ struct Board {
     h: usize,
     w: usize,
     v: Vec<Vec<Piece>>,
+    log: Vec<Operation>,
     me: Position,
     opp: Position,
     n: usize,
@@ -111,9 +206,9 @@ impl Display for Board {
         for i in 0..self.h {
             for j in 0..self.w {
                 if i == self.me.0 && j == self.me.1 {
-                    s.push('*');
+                    s.push('M');
                 } else if i == self.opp.0 && j == self.opp.1 {
-                    s.push('+');
+                    s.push('E');
                 } else {
                     match self.v[i][j] {
                         Empty => s.push('.'),
@@ -137,38 +232,77 @@ impl Board {
             h,
             w,
             v,
+            log: vec![],
             me,
             opp,
             n,
         }
     }
 
+    fn from_request(req: Request) -> Self {
+        let Request {
+            size: Point { x: h, y: w },
+            player_pos: Point { x: i, y: j },
+            ai_pos: Point { x, y },
+            board,
+        } = req;
+        let mut v = vec![vec![Empty; w]; h];
+        for i in 0..h {
+            for j in 0..w {
+                let piece = match board[i * w + j] {
+                    -1 => Empty,
+                    0 => Player(0),
+                    1 => Player(1),
+                    _ => unreachable!(),
+                };
+                v[i][j] = piece;
+            }
+        }
+        Self {
+            h,
+            w,
+            v,
+            log: vec![],
+            me: (x, y),
+            opp: (i, j),
+            n: 1,
+        }
+    }
+
     fn enumerate(&self) -> Vec<Operation> {
-        vec![Up, Down, Right, Left]
-            .into_iter()
-            .filter(|&op| {
-                let p = next_op(self.me, op, self.h, self.w);
-                if p.is_none() {
-                    return false;
+        let mut v = vec![Up, Down, Right, Left];
+        if self.log.len() >= 2 {
+            for i in 0..4 {
+                if v[i] == self.log[self.log.len() - 2] {
+                    v.swap(0, i);
                 }
-                let (i, j) = p.unwrap();
-                self.v[i][j] == Empty
+            }
+        }
+        v.into_iter()
+            .filter(|&op| {
+                next_op(self.me, op, self.h, self.w)
+                    .map(|(i, j)| self.v[i][j] == Empty)
+                    .unwrap_or(false)
             })
             .collect()
     }
 
     fn operate(&mut self, op: Operation) -> Result<(), &str> {
-        let opp = next_op(self.me, op, self.h, self.w);
-        if self.v[opp.0][opp.1] != Empty || opp.is_none() {
-            return Err("Not Empty!");
-        }
-        let opp = opp.unwrap();
-        let me = self.opp;
-        self.v[opp.0][opp.1] = Player(self.n);
-        self.n ^= 1;
-        self.opp = opp;
-        self.me = me;
-        Ok(())
+        next_op(self.me, op, self.h, self.w)
+            .ok_or("Don't sink into the wall!")
+            .and_then(|(i, j)| {
+                if self.v[i][j] != Empty {
+                    Err("Not Empty!")
+                } else {
+                    let me = self.opp;
+                    self.v[i][j] = Player(self.n);
+                    self.n ^= 1;
+                    self.opp = (i, j);
+                    self.me = me;
+                    self.log.push(op);
+                    Ok(())
+                }
+            })
     }
 
     fn calc_score(&self) -> isize {
@@ -207,7 +341,26 @@ impl Board {
         me_cnt - opp_cnt
     }
 
-    fn min_max_sub(&self, depth: usize, mut alpha: isize, beta: isize) -> isize {
+    fn rollback(&mut self) -> Result<(), &str> {
+        self.log
+            .pop()
+            .ok_or("No log!")
+            .and_then(|op| {
+                prev_op(self.opp, op, self.h, self.w).ok_or("Invalid operation in prev_op!")
+            })
+            .and_then(|(i, j)| {
+                let (x, y) = self.opp;
+                self.v[x][y] = Empty;
+                let opp = self.me;
+                let me = (i, j);
+                self.opp = opp;
+                self.me = me;
+                self.n ^= 1;
+                Ok(())
+            })
+    }
+
+    fn min_max_sub(&mut self, depth: usize, mut alpha: isize, beta: isize) -> isize {
         let v = self.enumerate();
 
         if depth == 0 || v.is_empty() {
@@ -215,9 +368,9 @@ impl Board {
         }
 
         for op in v {
-            let mut state = self.clone();
-            state.operate(op).unwrap();
-            let score = -state.min_max_sub(depth - 1, -beta, -alpha);
+            self.operate(op).unwrap();
+            let score = -self.min_max_sub(depth - 1, -beta, -alpha);
+            self.rollback().unwrap();
             alpha = alpha.max(score);
             if alpha > beta {
                 return alpha;
