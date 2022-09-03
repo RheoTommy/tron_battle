bfafb13dab30401d687a4e41bf254bc3834bcd46
RheoTommy <tommyrheo@gmail.com>
2022-09-04 00:52:03 +0900

    actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
-use crate::serve::open_server;
+use crate::game::Board;
+use actix_web::web::Json;
+use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
 pub mod game;
-pub mod serve;
 
-fn main() {
-    open_server();
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(handle))
+        .bind("localhost:6583")?
+        .run()
+        .await
+}
+
+#[post("/")]
+async fn handle(req: Json<Request>) -> impl Responder {
+    eprintln!("Access");
+    let state = Board::from_request(req.into_inner());
+    let op = state.min_max(10);
+    HttpResponse::Ok().json(op)
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Request {
+    pub size: Point,
+    pub player_pos: Point,
+    pub ai_pos: Point,
+    pub board: Vec<isize>,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
 }
